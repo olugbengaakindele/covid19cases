f1be1b170b46f3947591a86c2abf1fe8d9e8c18e
olugbenga Akindele <akindelegbenga@gmail.com>
2020-08-16 23:02:03 -0400

    added sates into combo box

--- usastatescovid.js
+++ usastatescovid.js
@@ -7,3 +7,31 @@
 //daily usa historical case
 //https://api.covidtracking.com/v1/us/daily.json
 
+const sts_cmb = document.querySelector("#state_select");
+
+loadEventListeners();
+
+function loadEventListeners(){
+
+}
+
+// xhr = new XMLHttpRequest();
+// xhr.open("GET","states.txt", true)
+// xhr.onload= function(){
+//     if (xhr.status === 200 ){
+//         console,log(xhr.status)
+//     }
+// }
+// xhr.send();
+
+
+
+//load states into combox;
+let all_states = []
+states.forEach(function(st){
+   all_states += `<option>${st.name}</option>`;
+}
+)
+sts_cmb.innerHTML = all_states;
+
+
